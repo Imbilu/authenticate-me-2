c0e7955d2bf06147099e6237434833711319b1ee
Imbilu <imbilusydney09@gmail.com>
2024-09-07 19:33:32 +0300

    implemented sign-in logic

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-            <form action="" className="flex flex-col gap-4">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                 <input
                     type="text"
                     placeholder="Username"
                     id="username"
                     className="bg-slate-100 p-3 rounded-lg"
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     placeholder="Email"
                     id="email"
                     className="bg-slate-100 p-3 rounded-lg"
+                    onChange={handleChange}
                 />
                 <input
                     type="password"
                     placeholder="Password"
                     id="password"
                     className="bg-slate-100 p-3 rounded-lg"
+                    onChange={handleChange}
                 />
                 <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">
                     Sign Up
