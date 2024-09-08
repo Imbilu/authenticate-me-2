f9661b4f2badae26659bb24d94ad441a53cf9887
Imbilu <imbilusydney09@gmail.com>
2024-09-08 16:43:25 +0300

    created update user API route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,40 @@
+import { request } from "express";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
     res.json({
         message: "user API",
     });
+};
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(
+            errorHandler(401, "You're not authorized to perform this action")
+        );
+    }
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    profilePic: req.body.profilePic,
+                },
+            },
+            { new: true }
+        );
+        const { password, ...user } = updatedUser._doc;
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 };
