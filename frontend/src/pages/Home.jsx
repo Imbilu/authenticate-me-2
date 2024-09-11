b8cb834bd72826ea3808fa58b523c25f10ab947b
Imbilu <imbilusydney09@gmail.com>
2024-09-11 14:33:29 +0300

    did home and about pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className="px-4 py-12 max-w-2xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-700">
+                Welcome to my Auth App!
+            </h1>
+            <p className="mb-4  text-slate-700">
+                This is a fullstack authentication app built with an express
+                server in the backend and connected to a MongoDB database. The
+                frontend is built using React and the redux toolkit for state
+                management. AUthentication is implemented using JSON Web token.
+                The app also has google OAuth allowing users to sign up and log
+                in with their google accounts.
+            </p>
+        </div>
+    );
 }
